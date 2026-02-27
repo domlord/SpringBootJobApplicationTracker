a159c8db912bc24f0bbc2120ed64006683cc62a6
dom lord <dominicrlord@gmail.com>
2026-02-27 19:30:15 +0000

    a

--- src/main/java/com/example/jobapplicationtracker/controller/JobApplicationController.java
+++ src/main/java/com/example/jobapplicationtracker/controller/JobApplicationController.java
@@ -1,8 +1,8 @@
 package com.example.jobapplicationtracker.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class JobApplicationController {
 
     public String
