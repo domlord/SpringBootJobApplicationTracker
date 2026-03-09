5f7bec71c43b84d4448a2d77b240c416a0f345b5
dom lord <dominicrlord@gmail.com>
2026-03-09 02:08:55 +0000

    begin controller

--- src/main/java/com/dom/jobapplicationtracker/controller/JobApplicationController.java
+++ src/main/java/com/dom/jobapplicationtracker/controller/JobApplicationController.java
@@ -1,10 +1,25 @@
 package com.dom.jobapplicationtracker.controller;
 
+import com.dom.jobapplicationtracker.controller.dto.CreateJobApplicationRequest;
+import com.dom.jobapplicationtracker.controller.dto.JobApplicationResponse;
+import com.dom.jobapplicationtracker.service.JobApplicationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/jobapplication")
 public class JobApplicationController {
 
-    public String
+    private final JobApplicationService service;
 
+    public JobApplicationController(JobApplicationService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<JobApplicationResponse> createJobApplication(
+            CreateJobApplicationRequest
+    )
 }
