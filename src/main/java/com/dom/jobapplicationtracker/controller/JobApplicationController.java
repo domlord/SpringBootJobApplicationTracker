ed3b9d4a1b7e852615f290db2204a5fa365a372e
dom lord <dominicrlord@gmail.com>
2026-03-31 21:43:15 +0100

    add controller components

--- src/main/java/com/dom/jobapplicationtracker/controller/JobApplicationController.java
+++ src/main/java/com/dom/jobapplicationtracker/controller/JobApplicationController.java
@@ -2,11 +2,14 @@ package com.dom.jobapplicationtracker.controller;
 
 import com.dom.jobapplicationtracker.controller.dto.CreateJobApplicationRequest;
 import com.dom.jobapplicationtracker.controller.dto.JobApplicationResponse;
+import com.dom.jobapplicationtracker.controller.dto.UpdateJobApplicationRequest;
 import com.dom.jobapplicationtracker.service.JobApplicationService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/jobapplication")
@@ -19,7 +22,35 @@ public class JobApplicationController {
     }
 
     @PostMapping
-    public ResponseEntity<JobApplicationResponse> createJobApplication(
-            CreateJobApplicationRequest
-    )
+    public ResponseEntity<JobApplicationResponse> createJobApplication(@Valid @RequestBody CreateJobApplicationRequest request) {
+        JobApplicationResponse response = service.createJobApplication(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+
+    @GetMapping
+    public List<JobApplicationResponse> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public JobApplicationResponse getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public JobApplicationResponse update( @PathVariable Long id,
+            @Valid @RequestBody UpdateJobApplicationRequest request) {
+        return service.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
