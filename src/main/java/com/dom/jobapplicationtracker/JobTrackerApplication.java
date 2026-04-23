fa29fb9a5811a11cb8e7fd72a9320cfcd12c13c0
dom lord <dominicrlord@gmail.com>
2026-04-23 20:09:44 +0100

    test

--- src/main/java/com/dom/jobapplicationtracker/JobTrackerApplication.java
+++ src/main/java/com/dom/jobapplicationtracker/JobTrackerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobTrackerApplication {
+public class JobTrackerApplication { test
     public static void main(String[] args) {
         SpringApplication.run(JobTrackerApplication.class, args);
     }
