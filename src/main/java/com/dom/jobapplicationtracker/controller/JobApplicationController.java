df42ba27528f88902784b4e6d2f21fc6b94fa326
dom lord <dominicrlord@gmail.com>
2026-05-03 20:17:10 +0100

    add summaries to each api operation

--- src/main/java/com/dom/jobapplicationtracker/controller/JobApplicationController.java
+++ src/main/java/com/dom/jobapplicationtracker/controller/JobApplicationController.java
@@ -4,6 +4,7 @@ import com.dom.jobapplicationtracker.controller.dto.CreateJobApplicationRequest;
 import com.dom.jobapplicationtracker.controller.dto.JobApplicationResponse;
 import com.dom.jobapplicationtracker.controller.dto.UpdateJobApplicationRequest;
 import com.dom.jobapplicationtracker.service.JobApplicationService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,7 @@ public class JobApplicationController {
         this.service = service;
     }
 
+    @Operation(summary = "Create a job application from json")
     @PostMapping
     public ResponseEntity<JobApplicationResponse> createJobApplication(@Valid @RequestBody CreateJobApplicationRequest request) {
         JobApplicationResponse response = service.createJobApplication(request);
@@ -31,22 +33,26 @@ public class JobApplicationController {
                 .body(response);
     }
 
+    @Operation(summary = "Get all stored job applications")
     @GetMapping
     public List<JobApplicationResponse> getAll() {
         return service.getAll();
     }
 
+    @Operation(summary = "Get job application with certain id")
     @GetMapping("/{id}")
     public JobApplicationResponse getById(@PathVariable Long id) {
         return service.getById(id);
     }
 
+    @Operation(summary = "Update job application with certain id")
     @PatchMapping("/{id}")
     public JobApplicationResponse update( @PathVariable Long id,
             @Valid @RequestBody UpdateJobApplicationRequest request) {
         return service.update(id, request);
     }
 
+    @Operation(summary = "Delete job application with certain id")
     @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, String>> delete(@PathVariable Long id) {
         service.delete(id);
